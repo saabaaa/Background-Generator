d3163f2045eef5b10941ff0c834f96690fa7feea
saabaaa <72267701+saabaaa@users.noreply.github.com>
2022-02-23 16:58:41 +0330

    Add files via upload

---
+++ backgraound/script.js
@@ -0,0 +1,29 @@
+//Selectors
+
+var css = document.querySelector("h4");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+    
+
+// function setGradient() {
+//     body.style.background = "linear-gradient(to left,"
+//     + color1.value 
+//     + ","
+//     + color2.value
+//     +")";
+// }
+
+function setGradient(){
+    body.style.background = "linear-gradient(to left,"
+    + color1.value
+    +","
+    + color2.value
+    ")";
+
+    css.textContent = body.style.background + ";"
+}
+  
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
